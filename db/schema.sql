7a47c38084abd42925d567d342daa4459bf970c3
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2023-06-28 20:53:14 +0200

    fix: fix deletion of users & products

--- db/schema.sql
+++ db/schema.sql
@@ -21,15 +21,15 @@ CREATE TABLE IF NOT EXISTS cart (
   user_id INTEGER NOT NULL,
   product_id INTEGER NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES users(id),
-  FOREIGN KEY (product_id) REFERENCES products(id)
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS orders (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
   user_id INTEGER NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES users(id)
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS order_items (
@@ -37,22 +37,22 @@ CREATE TABLE IF NOT EXISTS order_items (
   order_id INTEGER NOT NULL,
   product_id INTEGER NOT NULL,
   quantity INTEGER NOT NULL,
-  FOREIGN KEY (order_id) REFERENCES orders(id),
-  FOREIGN KEY (product_id) REFERENCES products(id)
+  FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
+  FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS verification (
   user_id INTEGER NOT NULL,
   token TEXT NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES users(id)
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS password_reset (
   user_id INTEGER NOT NULL,
   token TEXT NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES users(id)
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS shopping_list (
@@ -60,7 +60,7 @@ CREATE TABLE IF NOT EXISTS shopping_list (
   user_id INTEGER NOT NULL,
   name TEXT NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES users(id)
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 
@@ -68,7 +68,7 @@ CREATE TABLE IF NOT EXISTS shopping_list_items (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
   shopping_list_id INTEGER NOT NULL,
   product_id INTEGER NOT NULL,
-  FOREIGN KEY (shopping_list_id) REFERENCES shopping_list(id),
-  FOREIGN KEY (product_id) REFERENCES products(id)
+  FOREIGN KEY (shopping_list_id) REFERENCES shopping_list(id) ON DELETE CASCADE,
+  FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
 );
 
