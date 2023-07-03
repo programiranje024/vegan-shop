f85ec27278320670763bff3257044ecad3984d55
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2023-07-03 04:21:29 +0200

    feat: use bootstrap for navbar

--- www/views/header.php
+++ www/views/header.php
@@ -5,25 +5,45 @@ $logged_in = Session::isLoggedIn();
 <html>
 <head>
   <title>Vegan Shop</title>
-  <link rel="stylesheet" type="text/css" href="https://bootswatch.com/5/darkly/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="/css/main.css">
+  <link rel="stylesheet" type="text/css" href="https://bootswatch.com/5/united/bootstrap.min.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-  <nav>
-    <ul>
-      <li><a href="/index.php">Home</a></li>
-      <?php if ($logged_in) { ?>
-        <li><a href="/user/profile.php">Profile</a></li>
-        <li><a id='cart-link' href="/user/cart.php">Cart</a></li>
-        <li><a href="/user/shopping-lists.php">Shopping Lists</a></li>
-      <?php if (Session::getSessionUser()['admin']) { ?>
-        <li><a href="/admin/index.php">Admin</a></li>
-      <?php } ?>
-        <li><a href="/user/logout.php">Logout</a></li>
-      <?php } else { ?>
-        <li><a href="/user/login.php">Login</a></li>
-        <li><a href="/user/register.php">Register</a></li>
-      <?php } ?>
-    </ul>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="collapse navbar-collapse" id="mainNavbar">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="/index.php">Home</a>
+          </li>
+          <?php if ($logged_in) { ?>
+            <li class="nav-item">
+              <a class="nav-link" href="/user/profile.php">Profile</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/user/cart.php">Cart</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id='cart-link' href="/user/cart.php">Cart</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/user/shopping-lists.php">Shopping Lists</a>
+            </li>
+          <?php if (Session::getSessionUser()['admin']) { ?>
+            <li class="nav-item">
+              <a class="nav-link" href="/admin/index.php">Admin</a>
+            </li>
+          <?php } ?>
+            <li class="nav-item">
+              <a class="nav-link" href="/user/logout.php">Logout</a>
+            </li>
+          <?php } else { ?>
+            <li class="nav-item">
+              <a class="nav-link" href="/user/login.php">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/user/register.php">Register</a>
+            </li>
+          <?php } ?>
+        </ul>
+    </div>
   </nav>
