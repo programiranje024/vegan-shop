e56a4fafb22162351c09f854fd96d919d1aba10b
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2023-07-06 17:36:10 +0200

    fix responsive

--- www/views/header.php
+++ www/views/header.php
@@ -5,12 +5,16 @@ $logged_in = Session::isLoggedIn();
 <html>
 <head>
   <title>Vegan Shop</title>
-  <link rel="stylesheet" type="text/css" href="https://bootswatch.com/5/united/bootstrap.min.css">
+  <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <link rel="stylesheet" type="text/css" href="https://bootswatch.com/5/superhero/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="/css/main.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-3">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
     <div class="collapse navbar-collapse" id="mainNavbar">
         <ul class="navbar-nav">
           <li class="nav-item">
